1695f4dd6d78c2c7b22c2636c3a852b4dd5d9467
Plajer <plajer.xyz@gmail.com>
2020-07-03 20:39:16 +0200

    Remove text-muted from attribution

--- client/src/components/util/attribution.js
+++ client/src/components/util/attribution.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Attribution = () => {
     return <div className="small text-black-60" id="attribution">
-        Powered by <img alt='logo' src='https://cdn.feedbacky.net/static/img/logo.png' width='16px' height='16px'/> <a href='https://feedbacky.net' target='_blank' rel="noopener noreferrer" className="text-muted">Feedbacky</a>
+        Powered by <img alt='logo' src='https://cdn.feedbacky.net/static/img/logo.png' width='16px' height='16px'/> <a href='https://feedbacky.net' target='_blank' rel="noopener noreferrer" className="text-black-60">Feedbacky</a>
     </div>
 };
 
